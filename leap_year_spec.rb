0fbb521529864fce67b48767ce9a3e3ad4522216
tanay-incubyte <tanay@incubyte.co>
2025-01-02 09:22:48 +0000

    update tests

--- leap_year_spec.rb
+++ leap_year_spec.rb
@@ -2,8 +2,10 @@ require_relative 'leap_year'
 RSpec.describe 'Check for leap year' do
 
   it 'checks for leap year' do
-    expect(leap_year(2000)).to eq(true)
-    expect(leap_year(2001)).to eq(false)
+    expect(leap_year(2020)).to eq(true)
+    expect(leap_year(2021)).to eq(false)
+    expect(leap_year(2022)).to eq(false)
   end
+
 
 end

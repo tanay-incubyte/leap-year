1640af9785ba32e89ceef9f6b8dd4568e39c6e2c
tanay-incubyte <tanay@incubyte.co>
2025-01-02 09:37:05 +0000

    add tests to check for divisible by 100

--- leap_year_spec.rb
+++ leap_year_spec.rb
@@ -1,10 +1,19 @@
 require_relative 'leap_year'
 RSpec.describe 'Check for leap year' do
 
-  it 'checks for leap year' do
+  it 'checks for leap year with divisible by 4' do
     expect(leap_year(2020)).to eq(true)
+    expect(leap_year(2024)).to eq(true)
+  end
+  it 'checks for leap year with not divisible by 4' do
     expect(leap_year(2021)).to eq(false)
     expect(leap_year(2022)).to eq(false)
+    expect(leap_year(1994)).to eq(false) # it's my birth year so mandatory to check
+  end
+
+  it 'checks for leap year with divisible by 100' do
+    expect(leap_year(1800)).to eq(false)
+    expect(leap_year(1400)).to eq(false)
   end
 
 
